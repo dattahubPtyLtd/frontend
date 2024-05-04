00df97cb1343e96fdc0f541e8843c4778eadd2de
dattahubPtyLtd <nhthakur30@gmail.com>
2024-05-04 13:28:34 +1000

    info card fix

--- app/libs/InfoCards.tsx
+++ app/libs/InfoCards.tsx
@@ -1,8 +1,6 @@
-import { AlarmClockOff, ArrowDownNarrowWide, ArrowUpNarrowWide, LucideIcon } from "lucide-react";
 
 interface IInfoCard{
     title: string;
-    icon: LucideIcon;
     bodyText: string;
     imgSrc: string;
     id:number;
@@ -12,7 +10,6 @@ const infoCards: IInfoCard[] = [
 {
     title: "Roadmapping", 
     bodyText: "Our typical initial offering, this engagement offers you a deep-dive into assumptions and requirements, and gives you a solid understanding of your product, delivering both a roadmap and an estimate.",
-    icon: ArrowDownNarrowWide,
     imgSrc:"/roadmapping.jpeg",
     id:1
 },
@@ -23,14 +20,12 @@ const infoCards: IInfoCard[] = [
 {
     title: "Visual & UX Design",
     bodyText: "Turn specs and requirements into elegant, intuitive designs. Save money by ironing out the user experience before building it out in code. Our tech-minded designers strike the right balance of beautiful and pragmatic UIs.  ",
-    icon: ArrowUpNarrowWide,
     imgSrc:"/visual_uxdesign.png",
     id:2
 },
 {
     title: "Web & Mobile App Development", 
     bodyText: "Using proven, industry-standard technologies, we develop your product into a website and mobile application. With periodic demos from our team, we incorporate your feedback and iterate quickly.",
-    icon: AlarmClockOff,
     imgSrc:"/mobwebdev.png",
     id:3
 },
@@ -38,21 +33,18 @@ const infoCards: IInfoCard[] = [
 {
     title: "Project Management & Infrastructure", 
     bodyText: "We make sure the project is on track and on budget with monthly updates, efficient prioritization, and tracking using quality tools.",
-    icon: AlarmClockOff,
     imgSrc:"/iasspass.png",
     id:4
 },
 {
     title: "Ongoing Support", 
     bodyText: "With your product in use, we maintain it for you. This includes security updates, uptime monitoring, scaling and performance improvements as the product grows.",
-    icon: AlarmClockOff,
     imgSrc:"/support.png",
     id:5
 },
 {
     title: "Team Building & Hiring", 
     bodyText: "Our products are documented and built to grow. Feel the joy of easy onboarding. We help you hire your own staff, and are there to facilitate a smooth process.",
-    icon: AlarmClockOff,
     imgSrc:"/teambuilding.jpeg",
     id:6
 },
